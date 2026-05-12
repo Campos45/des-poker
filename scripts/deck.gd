57278fce85d86807cb90423e5c52520e9202a457
Martim <martim.elc@gmail.com>
2026-05-13 00:40:14 +0100

    commit checkpoint

--- scripts/deck.gd
+++ scripts/deck.gd
@@ -17,15 +17,13 @@ var suits = ["Copas", "Ouros", "Paus", "Espadas"]
 var ranks = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "Valete", "Dama", "Rei", "As"]
 var deck = []
 
-@onready var player_display = $PlayerHandDisplay
-@onready var community_display = $CommunityDisplay
-@onready var play_button = $PlayButton
-@onready var score_label = $ScoreLabel
+# Referência ao contentor que criaste na cena Main
+@onready var card_display = $CardDisplay
 
 func _ready():
 	randomize()
 	create_deck()
-	spawn_game_table() 
+	spawn_hand_visuals()
 
 func create_deck():
 	deck.clear()
@@ -37,70 +35,27 @@ func create_deck():
 			deck.append(new_card)
 	deck.shuffle()
 
-# Nova função de distribuir cartas (Substitui a antiga spawn_hand_visuals)
-func spawn_game_table():
-	# 1. Distribuir 5 Cartas Comunitárias para a Mesa
+# Esta função faz a magia de colocar as cartas no ecrã
+func spawn_hand_visuals():
+	# Tiramos 5 cartas do baralho
+	var my_hand = []
 	for i in range(5):
-		var card_data = deck.pop_front() 
+		my_hand.append(deck[i])
+	
+	# Para cada uma dessas 5 cartas:
+	# Para cada uma dessas 5 cartas:
+	for card_data in my_hand:
+		# 1. Criamos uma instância da cena visual
 		var new_card_visual = CARD_VISUAL_SCENE.instantiate()
-		community_display.add_child(new_card_visual)
-		new_card_visual.setup_card(card_data)
 		
-	# 2. Distribuir 7 Cartas para a Mão do Jogador
-	for i in range(7):
-		var card_data = deck.pop_front()
-		var new_card_visual = CARD_VISUAL_SCENE.instantiate()
-		player_display.add_child(new_card_visual)
-		new_card_visual.setup_card(card_data)
-
-func _on_play_button_pressed():
-	var selected_cards = []
-	var remaining_cards = [] # Cartas que vão para a Segunda Mão
-	
-	# 1. Analisar a prateleira do Jogador
-	for card_visual in player_display.get_children():
-		if card_visual.is_selected:
-			selected_cards.append(card_visual.card_data)
-			card_visual.queue_free() # Destrói as que jogaste!
-		else:
-			# As que NÃO selecionaste, ficam para a Segunda Mão
-			remaining_cards.append(card_visual.card_data)
-			
-	# 2. Analisar a prateleira da Mesa (Comunitárias)
-	for card_visual in community_display.get_children():
-		if card_visual.is_selected:
-			selected_cards.append(card_visual.card_data)
-			card_visual.toggle_selection() # Baixa-as (mas não as destrói)
-			
-		# Regra de Ouro: As comunitárias estão SEMPRE disponíveis para a 2ª Mão!
-		remaining_cards.append(card_visual.card_data)
-			
-	# Verificações de segurança
-	if selected_cards.size() > 5:
-		score_label.text = "Aviso: Seleciona no máximo 5 cartas combinadas!"
-		return
-	elif selected_cards.size() == 0:
-		score_label.text = "Aviso: Seleciona pelo menos 1 carta!"
-		return
+		# 2. Adicionamo-la como filha do HBoxContainer (para ficar alinhada)
+		card_display.add_child(new_card_visual)
 		
-	# --- CÁLCULO DA PRIMEIRA MÃO (A TUA JOGADA) ---
-	var hand1_data = HandEvaluator.evaluate_5_card_hand(selected_cards)
-	var score1_data = HandEvaluator.calculate_score(hand1_data)
+		# 3. Passamos os dados (rank, suit, especial) para o script da carta
+		new_card_visual.setup_card(card_data) # <--- A TUA LINHA 55 DEVE SER ESTA
 	
-	var final_text = "Primeira Mão: " + hand1_data["name"] + "\n"
-	final_text += str(score1_data["chips"]) + " Fichas X " + str(score1_data["mult"]) + " Mult = " + str(score1_data["total"]) + "\n\n"
+	# Opcional: Mostrar também a pontuação na consola para confirmar
+	var hand_data = HandEvaluator.evaluate_5_card_hand(my_hand)
+	var score_data = HandEvaluator.calculate_score(hand_data)
+	print("Mão Visualizada: ", hand_data["name"], " | Pontos: ", score_data["total"])
 	
-	# --- CÁLCULO DA SEGUNDA MÃO (A INTELIGÊNCIA ARTIFICIAL) ---
-	var hand2_data = HandEvaluator.find_best_hand(remaining_cards)
-	var score2_data = hand2_data["score_data"]
-	
-	final_text += "Segunda Mão (Auto): " + hand2_data["name"] + "\n"
-	final_text += str(score2_data["chips"]) + " Fichas X " + str(score2_data["mult"]) + " Mult = " + str(score2_data["total"]) + "\n\n"
-	
-	# --- GRANDE TOTAL ---
-	var grand_total = score1_data["total"] + score2_data["total"]
-	final_text += "GRANDE TOTAL DA RONDA: " + str(grand_total) + " Pontos!"
-	
-	score_label.text = final_text
-	
-	play_button.disabled = true
